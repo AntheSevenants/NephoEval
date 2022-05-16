8dc103e420f026893066fa5364e983d62bef8437
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-05-16 10:37:08 +0200

    Add count matrix processor class

---
+++ nepho_eval/count_matrix_processor.py
@@ -0,0 +1,43 @@
+from .matrix_processor import MatrixProcessor
+from .pipeline import Pipeline
+from tqdm.auto import tqdm
+from .helpers import create_filename
+
+import pandas as pd
+
+import shutil
+
+class CountMatrixProcessor(MatrixProcessor):
+    def process(self, do_reduce=False):
+        for lemma in tqdm(self.lemmas):
+            models = list(pd.read_csv(f"{TSV_PATH}/{lemma}/{lemma}.models.tsv", sep="\t")["_model"])
+    
+            for model in tqdm(models, leave=False):
+                prefix = f"{self.LEMMAS_PATH}/{lemma}/"
+                filename = f"{model}.tcmx.soc.pac"
+        
+                archive_path = f"{prefix}{filename}"
+                temp_path = f"{TEMP_PATH}{filename}"
+            
+                # Copy to temp first (I don't have permissions)
+                shutil.copyfile(archive_path, temp_path)
+        
+                # Load the TT-matrix
+                pipeline = Pipeline(temp_path)
+        
+                if do_reduce:
+                    pipeline.reduce(dim_tsne)
+            
+                pipeline.compute_distance_matrix()
+            
+                # Save the matrix in its entirety
+                pipeline.save_matrix(create_filename(filename, lemma, suffix="soc"))
+        
+                # Save the distance matrix
+                pipeline.save_distance_matrix(create_filename(filename, lemma, suffix="dist"))
+            
+                # Save token ids
+                pipeline.save_token_ids(create_filename(filename,
+                                                        lemma,
+                                                        suffix="ids",
+                                                        extension="tsv"))
