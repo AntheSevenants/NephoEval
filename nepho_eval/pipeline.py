6b7cf7168481bdcd3ac82e667fdeaac41a6bb77f
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-05-16 10:20:40 +0200

    Add Pipeline class

---
+++ nepho_eval/pipeline.py
@@ -0,0 +1,39 @@
+import json
+import numpy as np
+import pandas as pd
+from sklearn.metrics import pairwise_distances
+from nephosem import TypeTokenMatrix
+
+class Pipeline:
+    def __init__(self, soc_filename, is_bert=False):
+        if not is_bert:
+            self.tt_matrix = TypeTokenMatrix.load(soc_filename)
+            self.token_ids = self.tt_matrix.row_items
+            self.soc_matrix = self.tt_matrix.matrix.A
+        else:
+            self.soc_matrix = np.load(soc_filename)
+        
+        self.reduced_soc_matrix = None
+        
+    def load_token_ids(self, token_ids_filename):
+        with open(token_ids_filename, "rt") as reader:
+            self.token_ids = json.loads(reader.read())
+        
+    # Apply the given dimension reduction technique to this matrix
+    def reduce(self, dimension_reduction_technique):
+        self.reduced_soc_matrix = dimension_reduction_technique.reduce(self.soc_matrix)
+        
+    def compute_distance_matrix(self):
+        matrix = self.reduced_soc_matrix if self.reduced_soc_matrix is not None else self.soc_matrix
+        self.distance_matrix = pairwise_distances(matrix, metric="cosine")
+        
+    def save_matrix(self, filename):
+        np.save(filename, self.soc_matrix)
+    
+    def save_distance_matrix(self, filename):
+        np.save(filename, self.distance_matrix)
+        
+    def save_token_ids(self, filename):
+        data = { "_id": self.token_ids }
+        df = pd.DataFrame(data)
+        df.to_csv(filename, sep="\t", index=False)
